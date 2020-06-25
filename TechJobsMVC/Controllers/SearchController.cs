0e5651973840255cc7b67604f2ca1d1225aa6efb
Sung Hyun Park <45052946+psHyun7@users.noreply.github.com>
2020-06-25 14:08:27 -0400

    Finished Studio

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,9 +15,29 @@ namespace TechJobsMVC.Controllers
         public IActionResult Index()
         {
             ViewBag.columns = ListController.ColumnChoices;
+            ViewBag.radioCheck = "all";
             return View();
         }
 
         // TODO #3: Create an action method to process a search request and render the updated search view. 
+        [HttpPost]
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if (searchType == null || searchType == "")
+            {
+                searchType = "all";
+            }
+            if (searchTerm == null || searchTerm == "")
+            {
+                searchTerm = "all";
+            }
+
+            ViewBag.columns = ListController.ColumnChoices;
+            ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            ViewBag.radioCheck = searchType;
+
+            return View("index");
+
+        }
     }
 }
